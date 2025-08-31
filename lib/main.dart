77ffa68cc5b0f5f39060ed18191fa1ba8c226f43
khaled <khaledbahnawy@gmail.com>
2025-08-31 23:04:30 +0300

    add flutter screen util and use localization to make app language arabic with flutter util extention

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fruit_hub/core/routing/app_router.dart';
 import 'package:fruit_hub/core/routing/routes.dart';
+
+import 'generated/l10n.dart' show S;
 
 void main() {
   runApp(const FruitHub());
@@ -11,9 +15,22 @@ class FruitHub extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: Routes.splashView,
-      onGenerateRoute: AppRouter.onGenerateRoute);
+    return ScreenUtilInit(
+      minTextAdapt: true,
+      designSize: Size(375, 812),
+      child: MaterialApp(
+        localizationsDelegates: [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+        locale: Locale('ar'),
+        debugShowCheckedModeBanner: false,
+        initialRoute: Routes.splashView,
+        onGenerateRoute: AppRouter.onGenerateRoute,
+      ),
+    );
   }
 }
