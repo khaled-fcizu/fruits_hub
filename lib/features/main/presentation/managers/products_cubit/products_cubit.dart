cc0b93d4b7a30608df143a3d4460155478380a67
khaled <khaledbahnawy@gmail.com>
2025-09-15 23:53:15 +0300

    create products cubit

---
+++ lib/features/main/presentation/managers/products_cubit/products_cubit.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/core/entities/product_entity.dart';
+import 'package:fruit_hub/core/repos/product_repo.dart';
+
+part 'products_state.dart';
+
+class ProductsCubit extends Cubit<ProductsState> {
+  final ProductRepo _productsRepo;
+  ProductsCubit(this._productsRepo) : super(ProductsLoading());
+
+  Future<void> getProducts() async {
+    emit(ProductsLoading());
+    final result = await _productsRepo.getProducts();
+    result.fold(
+      (failure) => emit(ProductsFailure(message: failure.message)),
+      (products) => emit(ProductsSuccess(products: products)),
+    );
+  }
+
+  Future<void> getBestSellerProducts() async {
+    emit(ProductsLoading());
+    final result = await _productsRepo.getBestSellerProducts();
+    result.fold(
+      (failure) => emit(ProductsFailure(message: failure.message)),
+      (products) => emit(ProductsSuccess(products: products)),
+    );
+  }
+}
---
+++ lib/features/main/presentation/managers/products_cubit/products_state.dart
@@ -0,0 +1,17 @@
+part of 'products_cubit.dart';
+
+sealed class ProductsState {}
+
+final class ProductsInitial extends ProductsState {}
+
+final class ProductsLoading extends ProductsState {}
+
+final class ProductsFailure extends ProductsState {
+  final String message;
+  ProductsFailure({required this.message});
+}
+
+final class ProductsSuccess extends ProductsState {
+  final List<ProductEntity> products;
+  ProductsSuccess({required this.products});
+}
